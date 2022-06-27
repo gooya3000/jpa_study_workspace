c6992e6fccd3cfbcec7caf2a8cf9ade14a38241b
withardor0923@naver.com <withardor0923@naver.com>
2022-06-27 09:21:56 +0900

    1. app yml > 버전 문제로 MVCC 설정 제거 2. build gradle > 쿼리 파라미터 확인 라이브러리 추가 3. member test > 테스트 코드 작성

--- japshop/src/test/java/jpabook/japshop/MemberRepositoryTest.java
+++ japshop/src/test/java/jpabook/japshop/MemberRepositoryTest.java
@@ -1,8 +1,13 @@
 package jpabook.japshop;
 
 import com.fasterxml.jackson.databind.deser.std.StdKeyDeserializer;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +15,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberRepositoryTest {
     @Autowired MemberRepository memberRepository;
 
+    @Test
+    @Transactional
+    @Rollback(false) // @Test 이면서 @Transactional 이면 테스트 후 자동 롤백, 롤백을 해지하고 싶을 때 적어줌
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
 
 
 }
