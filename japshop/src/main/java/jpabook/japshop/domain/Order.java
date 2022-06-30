e6c9db0224ae962892a2a076c4bcd048eaabbeb3
withardor0923@naver.com <withardor0923@naver.com>
2022-07-01 08:56:23 +0900

    1. 지연로딩 추가, 연관관계 편의 매서드 추가

--- japshop/src/main/java/jpabook/japshop/domain/Order.java
+++ japshop/src/main/java/jpabook/japshop/domain/Order.java
@@ -16,14 +16,31 @@ public class Order {
     @GeneratedValue
     @Column(name="order_id")
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="member_id")
     private Member member;  // member-order 연관 관계 주인!
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
     private LocalDateTime orderDate;
+    @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    //==연관관계 (편의)메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+
 }
--- japshop/src/main/java/jpabook/japshop/domain/OrderItem.java
+++ japshop/src/main/java/jpabook/japshop/domain/OrderItem.java
@@ -12,10 +12,10 @@ public class OrderItem {
     @Id @GeneratedValue
     @Column(name="order_item_id")
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY) //기본은 EAGER(즉시로딩), 절대 쓰면 안됌.. 하나 조회할 때 모든 연관관계 데이터를 다 가져옴;;
     @JoinColumn(name = "order_id")
     private Order order;
     private int orderPrice;
